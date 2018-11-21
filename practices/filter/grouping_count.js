4f39141e9081f33a70ad084c9ee3a219676a7ca8
Mo Hon Sing <h.s.mo@oocl.com>
2018-11-21 14:44:27 +0800

    using lambda to grouping count

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -1,16 +1,22 @@
 'use strict';
 
 function grouping_count(collection) {
-  var items ={}
-  for(var elm = 0; elm< collection.length;elm++){
-    var key = collection[elm].toString()
-    if(isNaN(items[key])){
-      items[key]=1
-    }else{
-      items[key]++
-    }
-  }
-  return items
+  return collection.reduce((item, key) => {
+    item[key] = item[key] ? item[key] + 1 : 1;
+    return item;
+  }, Object.create(null));
+
+  // var items ={}
+  // for(var elm = 0; elm< collection.length;elm++){
+  //   var key = collection[elm].toString()
+  //   if(isNaN(items[key])){
+  //     items[key]=1
+  //   }else{
+  //     items[key]++
+  //   }
+  // }
+  // return items
 }
+
 
 module.exports = grouping_count;
