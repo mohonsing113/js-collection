b7d251def76a8068f0f8d008811c9929be1514c0
Mo Hon Sing <h.s.mo@oocl.com>
2018-11-21 14:38:15 +0800

    finished for loop version of grouping count

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -1,8 +1,16 @@
 'use strict';
 
 function grouping_count(collection) {
-
-  //implement here
+  var items ={}
+  for(var elm = 0; elm< collection.length;elm++){
+    var key = collection[elm].toString()
+    if(isNaN(items[key])){
+      items[key]=1
+    }else{
+      items[key]++
+    }
+  }
+  return items
 }
 
 module.exports = grouping_count;
